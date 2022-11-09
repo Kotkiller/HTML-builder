ed86467e7f5d51c4b60b907f7e4f86f845b730f4
Kotkiller <101184860+Kotkiller@users.noreply.github.com>
2022-11-09 21:30:50 +0300

    Update index.js

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,143 +1,137 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-const projectDist = path.join(__dirname, 'project-dist');
+try {
+    fs.promises.mkdir(
+        path.join(__dirname, "project-dist"),
+        {recursive: true},
+    )  
+      
+    fs.promises.mkdir(
+        path.join(__dirname, "project-dist", "assets"),
+        {recursive: true}
+    )
 
-fs.access(projectDist, error => {
-  if (error) {
-    createFolders();
-  } else {
-    recreateFiles();
-  };
-});
+} catch (err) {
+    if (err) throw err
+}
 
-function createFolders() {
-  const assetsDist = path.join(__dirname, 'assets');
-  const assetsCopyDist = path.join(projectDist, 'assets');
-
-  fs.mkdir(projectDist, { recursive: true }, err => {
-    if (err) throw err;
-    console.log(`-- Folder 'project-dist' created.`);
-
-    fs.mkdir(assetsCopyDist, { recursive: true }, err => {
-      if (err) throw err;
-      copyAssets(assetsDist);
-      console.log(`-- Folder 'assets' created.`);
-      createAndFillStyle();
-      createAndFillHTML();
-    });
-  });
-};
-
-function recreateFiles() {
-  fs.rm(projectDist, { recursive: true, force: true }, (err) => {
-    if (err) throw err;
-    console.log(`---------ALL files deleted---------`);
-    createFolders();
-  });
-};
-
-function copyAssets(pathFile) {
-  fs.readdir(pathFile, (err, files) => {
-    if (err) throw err;
-
-    files.forEach(file => {
-      const filePath = path.join(pathFile, file);
-
-      fs.stat(filePath, (err, stats) => {
+fs.writeFile(
+    path.join(__dirname, 'project-dist', 'index.html'),
+    "",
+    (err) => {
         if (err) throw err;
+    }
+)
 
-        if (stats.isDirectory()) {
-          createNewFolders(filePath, file);
-          copyAssets(filePath);
-        } else {
-          copyFiles(filePath, file);
-        };
-      });
-    });
-  });
-};
+fs.readdir(
+    path.join(__dirname, 'styles'),
+    {withFileTypes: true},
+    (err, files) => {
+        if(err) throw err;
+        fs.writeFile(
+            path.join(__dirname, 'project-dist', 'style.css'),
+             '',
+              (err)=> {
+                if(err) throw err;
+              })
+      
+        files.forEach((file => {
+            if(path.parse(path.join(__dirname, "styles", file.name)).ext === '.css') {
+                fs.readFile(
+                    path.join(__dirname, 'styles', file.name),
+                    'utf-8',
+                    (err, data) => {
+                        if(err) throw err;
+                        fs.appendFile(
+                            path.join(__dirname, 'project-dist', 'style.css'),
+                            data,
+                            (err) => {
+                                if(err) throw err
+                            }
+                        )
+                    }
 
-function createNewFolders(filePath, fileName) {
-  const newFolder = filePath.replace(/assets/gi, 'project-dist\\assets');
+                )
+            }
 
-  fs.mkdir(newFolder, err => {
-    if (err) throw err;
-    console.log(`------ Folder '${fileName}' created.`);
-  });
-};
+        }))
+    }
+)
 
-function copyFiles(originalPath, fileName) {
-  const fileCopy = originalPath.replace(/assets/gi, 'project-dist\\assets');
+fs.readdir(
+    path.join(__dirname, 'assets'),
+    {withFileTypes: true},
+    (err, files) => {
+        if(err) throw err
+        files.forEach((file) => {
+            if (!file.isFile()) {
+                fs.promises.mkdir(
+                path.join(__dirname, "project-dist", "assets", file.name),
+                {recursive: true}
+            ).then(
+                copyFile(file.name)
+            )
+                
+            };
+        })
+    }
+)
 
-  fs.copyFile(originalPath, fileCopy, err => {
-    if (err) throw err;
-    console.log(`-- ${fileName} - Copy completed.`);
-  });
-};
+function copyFile(dirname) {
+    fs.readdir(
+    path.join(__dirname, 'assets', dirname),
+    { withFileTypes: true},
+    (err, files) => {
+        if(err) throw err
+        files.forEach((file) => {
+            fs.copyFile(
+                path.join(__dirname, 'assets', dirname, file.name),
+                path.join(__dirname, 'project-dist', "assets", dirname, file.name),
+                (err) => {
+                    if (err) throw err
+                }
+            )
+        }
 
-function createAndFillStyle() {
-  const stylePath = path.join(projectDist, `style.css`);
-  const styles = path.join(__dirname, 'styles');
+        )
+    }
+    )
+}
 
-  fs.writeFile(stylePath, '', err => {
-    if (err) throw err;
-    console.log(`------ Creation and recording 'style.css'.`);
-
-    fs.readdir(styles, (err, files) => {
-      if (err) throw err;
-      files.forEach(file => {
-        const pathFile = path.join(styles, file);
-
-        if (path.extname(pathFile) === '.css') {
-          fs.readFile(pathFile, 'utf-8', (err, data) => {
-            if (err) throw err;
-
-            fs.appendFile(stylePath, `${data}\n`, err => {
-              if (err) throw err;
-              console.log(`------- Find and apply CSS style '${file}'`);
-            });
-          });
-        };
-      });
-    });
-  });
-};
-
-function createAndFillHTML() {
-  const mainHTML = path.join(projectDist, 'index.html');
-  const templateHTML = path.join(__dirname, 'template.html');
-  const dirComponentsHTML = path.join(__dirname, 'components');
-
-  fs.writeFile(mainHTML, '', err => {
-    if (err) throw err;
-    console.log(`------ Creation and recording 'index.html'.`);
-  });
-
-  fs.readFile(templateHTML, 'utf-8', (err, data) => {
-    if (err) throw err;
-    let dataHTML = data;
-
-    fs.readdir(dirComponentsHTML, async(err, files) => {
-      if (err) throw err;
-
-      for (let file of files) {
-        const fileName = file.replace(/.html/gi, '');
-        if (dataHTML.includes(`{{${fileName}}}`)) {
-          const fileComponents = path.join(dirComponentsHTML, file);
-
-          await fs.promises.readFile(fileComponents, 'utf-8')
-            .then(async fileData => {
-              dataHTML = dataHTML.replaceAll(`{{${fileName}}}`, fileData);
-
-              await fs.promises.writeFile(mainHTML, dataHTML)
-                .then(err => {
-                  if (err) throw err;
-                  console.log(`------- Find and apply HTML pattern '${file}'`);
-                });
-            });
-        };
-      };
-    });
-  });
-};
+fs.readFile(
+    path.join(__dirname, 'template.html'),
+    'utf-8',
+    (err, html) => {
+        if(err) throw err;
+        let htmlText = html;
+        fs.readdir(
+            path.join(__dirname, 'components'),
+            { withFileTypes: true},
+            (err, components) => {
+                if(err) throw err;
+                components.forEach((component, index) => {
+                    fs.readFile(
+                        path.join(__dirname, 'components', component.name),
+                        'utf-8',
+                        (err, componentInner) => {
+                            if(err) throw err;
+                            let componentFileName = component.name.split('.')[0];
+                            htmlText = htmlText.replace(`{{${componentFileName}}}`, componentInner);
+                            if(index == components.length-1) {
+                                fs.appendFile(
+                                    path.join(__dirname, 'project-dist', 'index.html'),
+                                    htmlText,
+                                    (err) => {
+                                        if(err) throw err;
+                                    }
+                                )
+                            }
+                            
+                        }
+                    )
+                })
+            }
+        )
+    }
+)
